a9f58a3c4a74b0c060fa3add68f60e6515a519e6
suyashpradhan <pradhansuyash39@gmail.com>
2021-07-10 23:49:42 +0530

    feat: ready for release v2

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,6 +9,11 @@ export const Home = () => {
         <Link to="/products">
           <img src={banner} alt="yoga_life" className="banner img-fluid"></img>
         </Link>
+        <img
+          src="https://yoga-trivia-assets.s3.ap-south-1.amazonaws.com/free-shipping.webp"
+          alt="free-shipping"
+          className="banner img-fluid"
+        ></img>
       </section>
     </>
   );
